dab62ea2a027daf39cb5d21e5213a434fccf5ebd
tsotnegujabidze <tsotnegujabidze3@gmail.com>
2024-07-13 18:15:41 +0400

    commit

---
+++ level20/Classwork/Classwork.py
@@ -0,0 +1,7 @@
+def display_info():
+    name = "Tsotne"
+    surname = "Gujabidze"
+    age = 14
+    print(f"My name is {name}. My surname is {surname}. My age is {age}.") 
+
+display_info()
---
+++ level20/Homework/Homework.py
@@ -0,0 +1,26 @@
+#დავალება N1
+def hello():
+    print("GOA Best!")
+
+hello()
+
+#დავალება N2
+def sum(num1, num2):
+    sum = num1 + num2
+    print(sum)
+sum(int(input("What is the first number?> ")), int(input("What is the second number?> ")))
+
+#დავალება N3
+def to_string(value):
+    print(str(value))
+to_string(int(input("What is the value?> ")))
+
+#დავალება N4
+def print_type(value):
+    print(type(value))
+print_type(input("What is the value?> "))
+
+#დავალება N5
+def to_string(value):
+    print(int(value))
+to_string(input("What is the value?> ")) 
