51c1d1122ad1b59877af0aae3f1fb637b65bd780
tsotnegujabidze <tsotnegujabidze3@gmail.com>
2024-07-13 18:06:02 +0400

    commit

---
+++ level16/Homework/Homework.py
@@ -0,0 +1,57 @@
+#დავალება N1
+family_list = ["Tsotne", "Ekaterine", "Valeri", "Miranda"]
+
+print(family_list[0])
+print(family_list[1])
+print(family_list[2])
+print(family_list[3])
+
+
+#დავალება N2
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numbers[0])
+print(numbers[9])
+
+#დავალება N3
+numbers_list = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+numbers_list[0] = 1
+numbers_list[2] = 1
+numbers_list[4] = 1
+numbers_list[6] = 1
+numbers_list[8] = 1
+numbers_list[10] = 1
+
+print(numbers_list)
+
+#დავალება N4
+
+information = []
+
+name = input("What is your name?> ")
+surname = input("What is your surname?> ")
+age = int(input("How old are you?> "))
+location = input("Where do you live?> ")
+gmail = input("What is your gmail?> ")
+
+information.append(name)
+information.append(surname)
+information.append(age)
+information.append(location)
+information.append(gmail)
+
+print(information)
+
+#დავალება N5
+surname = "Gujabidze"
+
+print(surname[0])
+print(surname[1])
+print(surname[2])
+print(surname[3])
+print(surname[4])
+print(surname[5])
+print(surname[6])
+print(surname[7])
+print(surname[8])
